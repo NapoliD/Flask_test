edfe8638de10082aa824d40e6e5c2e5d8b639a63
NapoliD <53539772+NapoliD@users.noreply.github.com>
2021-08-02 13:13:47 -0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 # # Python/Flask example
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export FLASK_APP=test.py
+export FLASK_APP=test2.py
 pip install -r requirements_apps.txt --user
 python -m flask run --host=0.0.0.0 --port=8888
